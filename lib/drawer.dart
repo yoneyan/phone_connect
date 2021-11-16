9f7d04aacc1cc65d8689d8b201ba45c7b8fd8b73
yoneyan <git@yoneyan.net>
2021-11-17 05:36:28 +0900

    [wip] 土台の作成

---
+++ lib/drawer.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:phone_connect/main.dart';
+import 'package:phone_connect/setting.dart';
+
+class AppDrawer extends StatefulWidget {
+  const AppDrawer({Key? key}) : super(key: key);
+
+  @override
+  State createState() {
+    return AppDrawerState();
+  }
+}
+
+class AppDrawerState extends State<AppDrawer> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: <Widget>[
+          const DrawerHeader(
+            child: Text(
+              '',
+              style: TextStyle(
+                fontSize: 24,
+                color: Colors.white,
+              ),
+            ),
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+          ),
+          ListTile(
+            title: const Text('Ble Device List'),
+            onTap: () {
+              Navigator.push(
+                context,
+                PageRouteBuilder(
+                  pageBuilder: (_, __, ___) => MainPage(),
+                  transitionDuration: const Duration(milliseconds: 500),
+                ),
+              );
+            },
+          ),
+          const Divider(),
+          ListTile(
+            title: const Text('Setting'),
+            onTap: () {
+              Navigator.push(
+                context,
+                PageRouteBuilder(
+                  pageBuilder: (_, __, ___) => SettingPage(),
+                  transitionDuration: const Duration(milliseconds: 500),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/setting.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'drawer.dart';
+
+class SettingPage extends StatefulWidget {
+  static const String roxuteName = '/setting';
+
+  @override
+  State<SettingPage> createState() => _SettingPage();
+}
+
+class _SettingPage extends State<SettingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Setting"),
+        ),
+        drawer: const AppDrawer(),
+        body: const Center(child: Text("Setting")));
+  }
+}
